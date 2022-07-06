ab71daca3b325da4efa847f74ba0d0f30d5bdd3b
ryu-yeon <rytak108@gmail.com>
2022-07-07 01:03:29 +0900

    create LEDBoard

--- LEDBoard/LEDBoard/AppDelegate.swift
+++ LEDBoard/LEDBoard/AppDelegate.swift
@@ -10,21 +10,35 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        sleep(2) //런치스크린 타임 조절 (아무것도안하고 시간만)
+        
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        //melon, youtube - 사용자 프리미엄 결제 했으면 play, 일시 정지
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        //youtube - 결제 안 한 사람에게 팝업창 띄워!
+        //kakaotalk -
+        //금융
     }
 
     // MARK: UISceneSession Lifecycle
-
+    
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- LEDBoard/LEDBoard/BoardViewController.swift
+++ LEDBoard/LEDBoard/BoardViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class BoardViewController: UIViewController {
 
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textColorButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        userTextField.placeholder = "내용을 작성해주세요."
+        userTextField.text = "안녕하세요"
+        userTextField.keyboardType = .emailAddress
+        
+        sendButton.setTitle("전송", for: .normal)
+        sendButton.setTitle("빨리보내!", for: .highlighted)
+        sendButton.backgroundColor = .yellow
+        sendButton.layer.cornerRadius = 5
+        sendButton.layer.borderWidth = 2
+        sendButton.layer.borderColor = UIColor.green.cgColor
+        sendButton.setTitleColor(.red, for: .normal)
+        sendButton.setTitleColor(.blue, for: .highlighted)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendButtonClicked(_ sender: UIButton) {
+        resultLabel.text = userTextField.text
     }
-    */
-
+    
+    @IBAction func textColorButtonClicked(_ sender: UIButton) {
+        resultLabel.textColor = .red
+    }
+    
+    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
