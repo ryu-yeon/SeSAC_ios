4993db1a61343de44fca4138dc9326cca39d9c3e
ryu-yeon <rytak108@gmail.com>
2022-07-13 14:40:16 +0900

    imageView cornerRadius 적용, datePikcer 설정

--- DayCalculator/DayCalculator/ViewController.swift
+++ DayCalculator/DayCalculator/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        }
+        designImageView()
     }
-
-
+    
+    func designImageView() {
+        imageView1.layer.cornerRadius = 20
+        imageView2.layer.cornerRadius = 20
+        imageView3.layer.cornerRadius = 20
+        imageView4.layer.cornerRadius = 20
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        
+    }
+    
 }
 
