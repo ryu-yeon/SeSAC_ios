b8389843ce11a289093622b776edc649f38dd9c4
ryu-yeon <rytak108@gmail.com>
2022-07-18 14:27:28 +0900

    Setting.storyboard 생성

---
+++ TrendMedia/TrendMedia/Case1TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Case1TableViewController.swift
+//  TrendMedia
+//
+//  Created by 유연탁 on 2022/07/18.
+//
+
+import UIKit
+
+class Case1TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
---
+++ TrendMedia/TrendMedia/Case2TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Case2TableViewController.swift
+//  TrendMedia
+//
+//  Created by 유연탁 on 2022/07/18.
+//
+
+import UIKit
+
+class Case2TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
