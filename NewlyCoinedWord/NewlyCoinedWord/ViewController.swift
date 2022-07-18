3ddbcc17afbf9c581f5fce7bf2c2236ae7f21687
ryu-yeon <rytak108@gmail.com>
2022-07-19 00:16:59 +0900

    Dictionary 대신 Enum을 사용하여 코드 구성

--- NewlyCoinedWord/NewlyCoinedWord/ViewController.swift
+++ NewlyCoinedWord/NewlyCoinedWord/ViewController.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+enum newlyWord: String {
+    case 삼귀다 = "연애를 시작하기 전 썸 단계!"
+    case 윰차 = "'유무차별'의 줄임말!"
+    case 실매 = "'실시간 매니저'의 줄임말!"
+    case 만반잘부 = "'만나서 반가워 잘 부탁해'의 줄임말!"
+    case 꾸안꾸 = "'꾸민 듯 안꾸민 듯'의 줄임말!"
+    case 구취 = "'구독 취소'의 줄임말!"
+    case 어쩔티비 = "'어쩌라고 안 물어봤는데'라는 뜻!"
+    case 킹받네 = "'열 받네'의 변형으로 매우 화가 난다는 뜻!"
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
@@ -16,9 +27,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var hashtagButton4: UIButton!
     
     @IBOutlet weak var resultLabel: UILabel!
-    
-    let newlyCoinedWord: [String: String] = ["삼귀다": "연애를 시작하기 전 썸 단계!", "윰차": "'유무차별'의 줄임말!", "실매": "'실시간 매니저'의 줄임말!", "만반잘부": "'만나서 반가워 잘 부탁해'의 줄임말!", "꾸안꾸": "'꾸민 듯 안꾸민 듯'의 줄임말!", "구취": "'구독 취소'의 줄임말!", "어쩔티비": "'어쩌라고 안 물어봤는데'라는 뜻!", "킹받네": "'열 받네'의 변형으로 매우 화가 난다는 뜻!"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,16 +53,53 @@ class ViewController: UIViewController {
     
     @IBAction func hashtagButtonClicked(_ sender: UIButton) {
         view.endEditing(true)
-        if let result = sender.currentTitle {
-            resultLabel.text = newlyCoinedWord[result] ?? "아직 몰라요...ㅠㅠ"
-            searchTextField.text = sender.currentTitle
+        
+        switch sender.currentTitle
+        {
+        case "삼귀다":
+            self.resultLabel.text = newlyWord.삼귀다.rawValue
+        case "윰차":
+            resultLabel.text = newlyWord.윰차.rawValue
+        case "실매":
+            self.resultLabel.text = newlyWord.실매.rawValue
+        case "만반잘부":
+            resultLabel.text = newlyWord.만반잘부.rawValue
+        case "꾸안꾸":
+            resultLabel.text = newlyWord.꾸안꾸.rawValue
+        case "구취":
+            self.resultLabel.text = newlyWord.구취.rawValue
+        case "어쩔티비":
+            resultLabel.text = newlyWord.어쩔티비.rawValue
+        case "킹받네":
+            resultLabel.text = newlyWord.킹받네.rawValue
+        default:
+            self.resultLabel.text = "아직 몰라요...ㅠㅠ"
         }
     }
     
     @IBAction func searchWord(_ sender: Any) {
         view.endEditing(true)
-        if let result = searchTextField.text {
-            resultLabel.text = newlyCoinedWord[result] ?? "아직 몰라요...ㅠㅠ"
+        
+        switch searchTextField.text
+        {
+        case "삼귀다":
+            self.resultLabel.text = newlyWord.삼귀다.rawValue
+        case "윰차":
+            resultLabel.text = newlyWord.윰차.rawValue
+        case "실매":
+            self.resultLabel.text = newlyWord.실매.rawValue
+        case "만반잘부":
+            resultLabel.text = newlyWord.만반잘부.rawValue
+        case "꾸안꾸":
+            resultLabel.text = newlyWord.꾸안꾸.rawValue
+        case "구취":
+            self.resultLabel.text = newlyWord.구취.rawValue
+        case "어쩔티비":
+            resultLabel.text = newlyWord.어쩔티비.rawValue
+        case "킹받네":
+            resultLabel.text = newlyWord.킹받네.rawValue
+        default:
+            self.resultLabel.text = "아직 몰라요...ㅠㅠ"
         }
     }
     
