c0f1aad10a4d7a6507aaacd21e2f3ea64f3bfd55
ryu-yeon <rytak108@gmail.com>
2022-07-20 00:52:07 +0900

    Search.storyboard UI 적용

---
+++ TrendMedia/TrendMedia/SearchTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTableViewCell.swift
+//  TrendMedia
+//
+//  Created by 유연탁 on 2022/07/20.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var summaryLabel: UILabel!
+    
+
+}
