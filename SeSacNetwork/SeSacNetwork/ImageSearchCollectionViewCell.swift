8a1e73c93ace4f91e37769bb9fe7a1ea544680d6
ryu-yeon <rytak108@gmail.com>
2022-08-03 22:03:31 +0900

    ImageSearchViewController 응답값 CollectioView에 출력

---
+++ SeSacNetwork/SeSacNetwork/ImageSearchCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSearchCollectionViewCell.swift
+//  SeSacNetwork
+//
+//  Created by 유연탁 on 2022/08/03.
+//
+
+import UIKit
+
+class ImageSearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
--- SeSacNetwork/SeSacNetwork/ImageSearchViewController.swift
+++ SeSacNetwork/SeSacNetwork/ImageSearchViewController.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 import Alamofire
+import Kingfisher
 import SwiftyJSON
 
 class ImageSearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    var list: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         fetchImage()
+        collectionViewLayout()
     }
     
     //fetchImage, requestImage, callREquestImage, getImage...
@@ -35,10 +42,47 @@ class ImageSearchViewController: UIViewController {
                 let json = JSON(value)
                 print("JSON: \(json)")
                 
+                for image in json["items"].arrayValue {
+                    
+                    self.list.append(image["link"].stringValue)
+                }
+                
+                self.collectionView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
     }
+}
 
+extension ImageSearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageSearchCollectionViewCell.reuseIdentifier, for: indexPath) as! ImageSearchCollectionViewCell
+        let url = URL(string: list[indexPath.item])
+        cell.imageView.kf.setImage(with: url)
+        
+        return cell
+    }
+    
+    func collectionViewLayout() {
+        
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - spacing * 2
+        
+        layout.itemSize = CGSize(width: width, height: width * 1.5)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumInteritemSpacing = spacing
+        layout.minimumLineSpacing = spacing
+        layout.scrollDirection = .vertical
+        
+        collectionView.collectionViewLayout = layout
+    }
+    
+    
 }
