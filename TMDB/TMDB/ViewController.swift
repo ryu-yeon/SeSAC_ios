55efaa60e7b31eb1adc3e964a772905c4432f303
ryu-yeon <rytak108@gmail.com>
2022-08-04 01:19:48 +0900

    json 정보 가져오기

--- TMDB/TMDB/ViewController.swift
+++ TMDB/TMDB/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        requestTMDB()
     }
 
+    func requestTMDB() {
+        let url = EndPoint.URL + "/all/day?api_key=" + APIKey.TMDB
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
