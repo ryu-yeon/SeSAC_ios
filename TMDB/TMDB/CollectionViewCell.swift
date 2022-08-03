025c96d7595e203a5ad7a18e3347d5ea15a23423
ryu-yeon <rytak108@gmail.com>
2022-08-04 01:20:43 +0900

    CollectionViewCell AutoLayout 적용

--- TMDB/TMDB/CollectionViewCell.swift
+++ TMDB/TMDB/CollectionViewCell.swift
@@ -9,4 +9,8 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
 }
