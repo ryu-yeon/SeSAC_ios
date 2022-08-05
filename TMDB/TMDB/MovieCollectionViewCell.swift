bd35b52130bb231a1d74dbe8c0c3101a8026511b
ryu-yeon <rytak108@gmail.com>
2022-08-06 06:50:41 +0900

    UI수정

---
+++ TMDB/TMDB/MovieCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCollectionViewCell.swift
+//  TMDB
+//
+//  Created by 유연탁 on 2022/08/03.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var castLabel: UILabel!
+    @IBOutlet weak var mediaView: UIView!
+    
+}
