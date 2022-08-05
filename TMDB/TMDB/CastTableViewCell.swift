3ee9e5b42afdb7ce55c17f7a65aa6003ce35e3ae
ryu-yeon <rytak108@gmail.com>
2022-08-06 06:51:18 +0900

    CastTableView 추가

---
+++ TMDB/TMDB/CastTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CastTableViewCell.swift
+//  TMDB
+//
+//  Created by 유연탁 on 2022/08/05.
+//
+
+import UIKit
+
+class CastTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var castImageView: UIView!
+    @IBOutlet weak var castNameLabel: UILabel!
+    @IBOutlet weak var castNicknameLabel: UILabel!
+    
+}
