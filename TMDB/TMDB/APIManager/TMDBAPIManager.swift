604bc59858ab70998202e7320216299e7526de14
ryu-yeon <rytak108@gmail.com>
2022-08-07 11:17:26 +0900

    TMDBAPIManager 적용

--- TMDB/TMDB/APIManager/TMDBAPIManager.swift
+++ TMDB/TMDB/APIManager/TMDBAPIManager.swift
@@ -5,82 +5,81 @@
 //  Created by 유연탁 on 2022/08/05.
 //
 
-//import UIKit
-//
-//import Alamofire
-//import SwiftyJSON
-//
-//class TMDBAPIManager {
-//    
-//    static let shared = TMDBAPIManager()
-//    
-//    private init() {}
-//    
-//    typealias completionHandler = (Int, [Int], [MediaModel]) -> Void
-//    
-//    func requestTrending(page: Int, completionHandler: @escaping completionHandler) {
-//        let url = EndPoint.TrendURL + "/movie/day?api_key=" + APIKey.TMDB + "&page=\(page)"
-//        AF.request(url, method: .get).validate().responseData { response in
-//            switch response.result {
-//            case .success(let value):
-//                let json = JSON(value)
-////                print("JSON: \(json)")
-//                
-////                var list: [MediaModel] = []
-////                var movieList: [Int] = []
-////
-////                for media in json["results"].arrayValue {
-////                    let title = media["title"].stringValue
-////                    let imageURL = media["backdrop_path"].stringValue
-////                    let overview = media["overview"].stringValue
-////                    let releaseDate = media["release_date"].stringValue
-////                    let genre = media["genre_ids"].arrayValue.map { $0.intValue}
-//                  //.map {$0.intvalue}
-//                    movieList.append(media["id"].intValue)
-////                    requestMovie(movieId: media["id"].intValue)
-////                    let data = MediaModel(title: title, imageURL: imageURL, overview: overview, releaseDate: releaseDate, genre: genre)
-////                    print(genre)
-////                    list.append(data)
-//                }
-////                for movie in movieList {
-////                    self.requestMovie(movieId: movie)
-////                }
-//                let totalCount = json["total_pages"].intValue
-//                
-//                
-//                completionHandler(totalCount, movieList, list)
-//                
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
-//    
-//    func requestMovie(movieId: Int, completionHandler2: [[String]]) -> Void {
-//        let url = EndPoint.MovieURL + "/\(movieId)/credits?api_key=" + APIKey.TMDB + "&language=en-US"
-//        AF.request(url, method: .get).validate().responseData { response in
-//            switch response.result {
-//            case .success(let value):
-//                let json = JSON(value)
-//                
-//                var castList: [[String]] = []
-//    
-//                var casts: [String] = []
-//                
-//                for person in json["cast"].arrayValue {
-//                    casts.append(person["name"].stringValue)
-//                }
-//                castList.append(casts)
-////                print(self.castList[0])
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//
-//    }
-//    
-//    
-//    
-//    
-//}
-//        
+import UIKit
+
+import Alamofire
+import SwiftyJSON
+
+class TMDBAPIMagnager {
+    
+    static let shared = TMDBAPIMagnager()
+    
+    private init() {}
+    
+    typealias completionHandler = ([MovieModel], Int) -> Void
+    typealias completionHandler2 = ([CastModel]) -> Void
+    
+    func requestTrendingData(startPage: Int, completionHandler: @escaping completionHandler) {
+        let url = EndPoint.TrendURL + "/movie/day?api_key=" + APIKey.TMDB + "&page=\(startPage)"
+        AF.request(url, method: .get).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+
+                var list: [MovieModel] = []
+                
+                for media in json["results"].arrayValue {
+                    let title = media["title"].stringValue
+                    let imageURL = media["backdrop_path"].stringValue
+                    let overview = media["overview"].stringValue
+                    let releaseDate = media["release_date"].stringValue
+                    let genre = media["genre_ids"].arrayValue.map {$0.intValue}
+                    let id = media["id"].intValue
+                    let posterURL = media["poster_path"].stringValue
+
+                    let data = MovieModel(id: id, posterURL: posterURL, title: title, imageURL: imageURL, overview: overview, releaseDate: releaseDate, genre: genre)
+                    
+                    list.append(data)
+                }
+
+                let totalCount = json["total_pages"].intValue
+
+                completionHandler(list, totalCount)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func requestMovieData(movieId: Int, compeltionHandeler2: @escaping completionHandler2) {
+        let url = EndPoint.MovieURL + "/\(movieId)/credits?api_key=" + APIKey.TMDB + "&language=en-US"
+        AF.request(url, method: .get).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                var castList: [CastModel] = []
+                
+                var casts: [String] = []
+                var characters: [String] = []
+                var imageURL: [String] = []
+                
+                for person in json["cast"].arrayValue {
+                    casts.append(person["name"].stringValue)
+                    characters.append(person["character"].stringValue)
+                    imageURL.append(person["profile_path"].stringValue)
+                }
+                
+                let data = CastModel(casts: casts, characters: characters, imageURL: imageURL)
+                
+                castList.append(data)
+                               
+                compeltionHandeler2(castList)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
--- TMDB/TMDB/TrendingViewController.swift
+++ TMDB/TMDB/TrendingViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-import Alamofire
 import Kingfisher
-import SwiftyJSON
 
 class TrendingViewController: UIViewController {
 
@@ -29,7 +27,7 @@ class TrendingViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: nil)
         navigationItem.backButtonTitle = " "
         
-        requestTrending(page: startPage)
+        requestTrending(startPage: startPage)
         
         collectionView.register(UINib(nibName: "MovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: MovieCollectionViewCell.reusableidentifier)
         collectionView.dataSource = self
@@ -38,71 +36,24 @@ class TrendingViewController: UIViewController {
         setColletionViewLayout()
         
     }
-    
 
-    func requestTrending(page: Int) {
-        let url = EndPoint.TrendURL + "/movie/day?api_key=" + APIKey.TMDB + "&page=\(page)"
-        AF.request(url, method: .get).validate().responseData { [self] response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-//                print("JSON: \(json)")
-                for media in json["results"].arrayValue {
-                    let title = media["title"].stringValue
-                    let imageURL = media["backdrop_path"].stringValue
-                    let overview = media["overview"].stringValue
-                    let releaseDate = media["release_date"].stringValue
-                    let genre = media["genre_ids"].arrayValue.map {$0.intValue}
-                    let id = media["id"].intValue
-                    let posterURL = media["poster_path"].stringValue
-
-//                    self.movieList.append(id)
+    func requestTrending(startPage: Int) {
+        
+        TMDBAPIMagnager.shared.requestTrendingData(startPage: startPage) { list, totalCount in
+            self.list.append(contentsOf: list)
+            self.totalCount = totalCount
+            for number in 20 * (self.startPage - 1)..<20 * self.startPage {
+                TMDBAPIMagnager.shared.requestMovieData(movieId: list[number].id) { castList in
+                    self.castList.append(contentsOf: castList)
                     
-                    let data = MovieModel(id: id, posterURL: posterURL, title: title, imageURL: imageURL, overview: overview, releaseDate: releaseDate, genre: genre)
-                    self.list.append(data)
+                    DispatchQueue.main.async {
+                        if self.castList.count == self.list.count {
+                            self.collectionView.reloadData()
+                        }
+                    }
                 }
-                
-                for number in (20 * (startPage - 1))..<(20 * startPage) {
-                    self.requestMovie(movieId: list[number].id)
-                }
-                self.totalCount = json["total_pages"].intValue
-
-            case .failure(let error):
-                print(error)
             }
         }
-    }
-    
-    func requestMovie(movieId: Int) {
-        let url = EndPoint.MovieURL + "/\(movieId)/credits?api_key=" + APIKey.TMDB + "&language=en-US"
-        AF.request(url, method: .get).validate().responseData { [self] response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-
-                var casts: [String] = []
-                var characters: [String] = []
-                var imageURL: [String] = []
-                
-                for person in json["cast"].arrayValue {
-                    casts.append(person["name"].stringValue)
-                    characters.append(person["character"].stringValue)
-                    imageURL.append(person["profile_path"].stringValue)
-                }
-                
-                let data = CastModel(casts: casts, characters: characters, imageURL: imageURL)
-                self.castList.append(data)
-                
-                if self.castList.count == self.list.count {
-                    collectionView.reloadData()
-                }
-                
-                
-            case .failure(let error):
-                print(error)
-            }
-        }
-
     }
 }
 
@@ -122,7 +73,6 @@ extension TrendingViewController: UICollectionViewDelegate, UICollectionViewData
         cell.titleLabel.font = .systemFont(ofSize: 16, weight: .bold)
         
         var casts = ""
-        
         for cast in castList[indexPath.item].casts {
             casts += "\(cast)"
             if cast == castList[indexPath.item].casts.last {
@@ -130,10 +80,11 @@ extension TrendingViewController: UICollectionViewDelegate, UICollectionViewData
             }
             casts += ", "
         }
-        
+
         cell.castLabel.text = casts
         cell.castLabel.font = .systemFont(ofSize: 12, weight: .regular)
         cell.castLabel.textColor = .gray
+        
         cell.dateLabel.text = list[indexPath.item].releaseDate
         cell.dateLabel.font = .systemFont(ofSize: 12, weight: .semibold)
         cell.dateLabel.textColor = .gray
@@ -196,15 +147,13 @@ extension TrendingViewController: UICollectionViewDataSourcePrefetching {
             if list.count - 1 == indexPath.item && startPage < totalCount{
                 startPage += 1
 
-                requestTrending(page: startPage)
+                requestTrending(startPage: startPage)
             }
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
-        print("====취소:\(indexPaths)")
     }
-    
 }
 
 extension TrendingViewController: ComponentProductCellDelegate {
@@ -214,7 +163,6 @@ extension TrendingViewController: ComponentProductCellDelegate {
         vc.movieTitle = list[index].title
         vc.movieId = list[index].id
         navigationController?.pushViewController(vc, animated: true)
-        
     }
 }
 
