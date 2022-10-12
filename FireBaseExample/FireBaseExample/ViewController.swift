5924bdaf29fe860a7cedb4e78ab6c06dca981773
ryu-yeon <rytak108@gmail.com>
2022-10-12 14:29:13 +0900

    day68 실습

---
+++ FireBaseExample/FireBaseExample/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  FireBaseExample
+//
+//  Created by 유연탁 on 2022/10/05.
+//
+
+import UIKit
+
+import FirebaseAnalytics
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        Analytics.logEvent("rejack", parameters: [
+//          "name": "고래밥",
+//          "full_text": "안녕하세요",
+//        ])
+//
+//        Analytics.setDefaultEventParameters([
+//          "level_name": "Caverns01",
+//          "level_difficulty": 4
+//        ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController ViewWillAppear")
+    }
+    
+    @IBAction func crashClikcede(_ sender: Any) {
+    }
+    
+    @IBAction func settingButtonClicked(_ sender: UIButton) {
+        navigationController?.pushViewController(SettingViewController(), animated: true)
+    }
+    
+    @IBAction func profileButtonClicked(_ sender: UIButton) {
+        present(ProfileViewController(), animated: true)
+    }
+}
+
+extension UIViewController {
+    
+    var topViewController: UIViewController? {
+        return self.topViewController(currentViewController: self)
+    }
+    
+    //최상위 뷰컨트롤러를 판단해주는 메서드
+    func topViewController(currentViewController: UIViewController) -> UIViewController {
+        if let tabBarController = currentViewController as? UITabBarController, let selectedViewController = tabBarController.selectedViewController {
+            
+            return self.topViewController(currentViewController: selectedViewController)
+        } else if let navigationController = currentViewController as? UINavigationController, let visibleViewController = navigationController.visibleViewController {
+            
+            return self.topViewController(currentViewController: visibleViewController)
+        } else if let presentedViewController = currentViewController.presentedViewController {
+            
+            return self.topViewController(currentViewController: presentedViewController)
+        } else {
+            return currentViewController
+        }
+    }
+}
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ProfileViewController ViewWillAppear")
+    }
+}
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("SettingViewController ViewWillAppear")
+    }
+}
+
+extension UIViewController {
+    
+    //class vs static
+    class func swizzleMethod() {
+        
+        let origin = #selector(viewWillAppear)
+        let change = #selector(changeViewWillAppear)
+        
+        guard let originMethod = class_getInstanceMethod(UIViewController.self, origin), let changeMethod = class_getInstanceMethod(UIViewController.self, change) else {
+            print("함수를 찾을 수 없거나 오류 발생")
+            return
+        }
+        
+        method_exchangeImplementations(originMethod, changeMethod)
+        
+    }
+    
+    @objc func changeViewWillAppear() {
+        print("Change ViewWillAppear SUCCEED")
+    }
+}
